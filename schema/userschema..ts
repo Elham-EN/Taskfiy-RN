801790899aec16fddc2ac6c8260e436a9d1cf0aa
Elham-EN <ilhamrezaie@gmail.com>
2024-04-20 13:46:55 +1000

    created stronger validation rule requirment for the password field to accept special chars and numners

--- schema/userschema..ts
+++ schema/userschema..ts
@@ -4,5 +4,20 @@ import { SignupFormData } from "../types/formDataTypes";
 export const UserSchema: ZodType<SignupFormData> = z.object({
   fullname: z.string(),
   email: z.string().email(),
-  password: z.string().min(8, { message: "Password must at lest have 8 characters" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must at lest have 8 characters" })
+    .refine(
+      (password) => {
+        const hasUpperCase = /[A-Z]/.test(password);
+        const hasLowerCase = /[a-z]/.test(password);
+        const hasNumber = /[0-9]/.test(password);
+        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+        return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+      },
+      {
+        message:
+          "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.",
+      }
+    ),
 });
